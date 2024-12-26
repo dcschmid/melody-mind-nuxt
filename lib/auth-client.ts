8836eebd324f058a4cee99e19a48a4b49e7b48b4
dcschmid <daniel.schmid1980@icloud.com>
2024-12-26 11:23:12 +0100

    fix: small auth fix

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,4 +1,3 @@
-import { usernameClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/vue";
 
 export const authClient = createAuthClient();
--- server/api/[...auth].ts
+++ server/api/[...auth].ts
@@ -1,4 +1,4 @@
-import { auth } from "~/utils/auth.config";
+import { auth } from "~/lib/auth";
 
 export default defineEventHandler((event) => {
   return auth.handler(toWebRequest(event));
